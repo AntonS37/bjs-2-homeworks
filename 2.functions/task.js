93675b41bdfed090e55b2762ada4f99bb7860ce2
Neto <5308014@mail.ru>
2022-12-29 19:32:54 +0300

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,82 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg;
+ 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum = sum + arr[i];
+  }
 
+  avg = parseFloat((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+  let sum = 0;
+  if (arr.length > 0) {
+    for (let i = 0; i < arr.length; i++) {
+      sum = sum + arr[i];
+    }
+  }
+  return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  let difference;
+  if (arr.length > 0) {
+    difference = Math.max.apply(null, arr) - Math.min.apply(null, arr);
+  } else {
+    difference = 0;
+  }
+  return difference;
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  let sumEven = 0;
+  let sumOdd = 0;
+  if (arr.length > 0) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        sumEven = sumEven + arr[i];
+      } else {
+        sumOdd = sumOdd + arr[i];
+      }
+    }
+    return sumEven - sumOdd;
+  } else {
+    return 0;
+  }
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  if (arr.length > 0) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        sumEvenElement = sumEvenElement + arr[i];
+        countEvenElement++;
+      }
+    }
+    return sumEvenElement / countEvenElement;
+  } else {
+    return 0;
+  }
 }
 
 function makeWork (arrOfArr, func) {
-
+  let maxWorkerResult = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const resultFunc = func(...arrOfArr[i]);
+    if (resultFunc > maxWorkerResult) {
+      maxWorkerResult = resultFunc;
+    }
+  }
+  return maxWorkerResult;
 }
